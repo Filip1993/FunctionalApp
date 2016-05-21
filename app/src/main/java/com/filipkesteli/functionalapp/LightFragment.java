62cb6dce4619d362c88dc19938e021a1d45178e6
Filip <filip.kesteli@gmail.com>
2016-05-21 11:07:41 +0200

    ***FLOATING ACTION BAR*** -Setting FloatingActionButton clickable

--- app/src/main/java/com/filipkesteli/functionalapp/LightFragment.java
+++ app/src/main/java/com/filipkesteli/functionalapp/LightFragment.java
@@ -2,7 +2,9 @@ package com.filipkesteli.functionalapp;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class LightFragment extends Fragment {
 
+    private FloatingActionButton fabDashboard;
 
     public LightFragment() {
         // Required empty public constructor
@@ -23,7 +26,7 @@ public class LightFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_light, container, false);
+        View v = inflater.inflate(R.layout.fragment_light, container, false);
+        return v;
     }
-
 }
--- app/src/main/java/com/filipkesteli/functionalapp/MainActivity.java
+++ app/src/main/java/com/filipkesteli/functionalapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.filipkesteli.functionalapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private FloatingActionButton fabDashboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
         initWidgets();
         connectTabWithPager();
+        setupListeners();
     }
 
+    //Connecting views with private variables:
     private void initWidgets() {
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        fabDashboard = (FloatingActionButton) findViewById(R.id.fabDashboard);
     }
 
+    //setup EVENT LISTENERS
+    private void setupListeners() {
+        fabDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActionBar actionBar = getSupportActionBar();
+                if (actionBar.isShowing()) {
+                    actionBar.hide();
+                } else {
+                    actionBar.show();
+                }
+            }
+        });
+    }
+
+    //Connecting TabLayout with ViewPager:
     private void connectTabWithPager() {
         //Adapter object for viewPager:
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -31,5 +57,36 @@ public class MainActivity extends AppCompatActivity {
 
         //Connection of viewPager with tabLayout -> simple method:
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    //powerfull overrided method that tells us which ActionBar menu we will use
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater -> Returns a MenuInflater with this context
+        MenuInflater menuInflater = getMenuInflater();
+        //We are inflating menu layout with menu variable
+        menuInflater.inflate(R.menu.menu, menu);
+        //onCreateOptionsMenu returns true if menu is to be displayed
+        return true;
+    }
+
+    //Depending which item is selected, do something
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                break;
+            case R.id.action_favorite:
+                break;
+            case R.id.action_home:
+                break;
+            case R.id.action_list:
+                break;
+            case R.id.action_profile:
+                break;
+            case R.id.action_search:
+                break;
+        }
+        return true;
     }
 }
