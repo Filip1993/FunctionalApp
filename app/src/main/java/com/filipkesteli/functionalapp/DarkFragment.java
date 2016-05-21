f5515e1cf118d98ef3f623d5959894cf03c52995
Filip <filip.kesteli@gmail.com>
2016-05-21 08:34:48 +0200

    ADDING FRAGMENTS: ***Pure fragments -> New>Fragment>with Layout

---
+++ app/src/main/java/com/filipkesteli/functionalapp/DarkFragment.java
@@ -0,0 +1,29 @@
+package com.filipkesteli.functionalapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DarkFragment extends Fragment {
+
+
+    public DarkFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dark, container, false);
+    }
+
+}
