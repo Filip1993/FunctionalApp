3c66b1ec133ad719e9444d359246375b56376357
Filip <filip.kesteli@gmail.com>
2016-05-21 08:01:17 +0200

    Initial commit

---
+++ app/src/main/java/com/filipkesteli/functionalapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.filipkesteli.functionalapp;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initWidgets();
+        setupTabs();
+    }
+
+    private void initWidgets() {
+
+    }
+
+    private void setupTabs() {
+
+    }
+}
