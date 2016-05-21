0e10cef1304c86bd4b0f58c532d3e608bb07902f
Filip <filip.kesteli@gmail.com>
2016-05-21 09:08:34 +0200

    ***Main Activity*** -methods: initWidgets(), connectTabWithPager() -using ViewPagerAdapter class object and setAdapter method -setupWithViewPager -> the most powerful method till now -> it connects TabLayout with ViewPager

--- app/src/main/java/com/filipkesteli/functionalapp/MainActivity.java
+++ app/src/main/java/com/filipkesteli/functionalapp/MainActivity.java
@@ -1,13 +1,14 @@
 package com.filipkesteli.functionalapp;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initWidgets();
-        setupTabs();
+        connectTabWithPager();
     }
 
     private void initWidgets() {
-
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
     }
 
-    private void setupTabs() {
+    private void connectTabWithPager() {
+        //Adapter object for viewPager:
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
 
+        //Connection of viewPager with tabLayout -> simple method:
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
